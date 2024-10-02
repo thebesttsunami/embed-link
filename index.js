a8e4a1b5a9e39fbf216e68e6da96d8a8a46086dd
fabbyan <162686236+thebesttsunami@users.noreply.github.com>
2024-10-02 16:08:59 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+<script>
+  window.open("https://sites.google.com/view/yuvens/yuvens-beranda-public" "_blank")
+  document.write("https://sites.google.com/view/yuvens/yuvens-beranda-public")
+  </script>
